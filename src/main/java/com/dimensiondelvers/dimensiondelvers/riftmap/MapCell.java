f52093d2ab4ceca341f3fb1d4385449b03399e93
Anorak01 <adamfri007@gmail.com>
2025-02-08 00:05:24 +0100

    Minimal code to draw I spent more than an hour on this headache, curse you NeoForge update

---
+++ src/main/java/com/dimensiondelvers/dimensiondelvers/riftmap/MapCell.java
@@ -0,0 +1,13 @@
+package com.dimensiondelvers.dimensiondelvers.riftmap;
+
+public class MapCell {
+    public int x;
+    public int y;
+    public int z;
+
+    public MapCell(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
---
+++ src/main/java/com/dimensiondelvers/dimensiondelvers/riftmap/RiftMap.java
@@ -0,0 +1,56 @@
+package com.dimensiondelvers.dimensiondelvers.riftmap;
+
+import com.dimensiondelvers.dimensiondelvers.events.RenderEvents;
+import com.google.common.eventbus.Subscribe;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.*;
+import net.minecraft.client.renderer.GameRenderer;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.EventPriority;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.RenderGuiEvent;
+import net.neoforged.neoforge.client.event.RenderTooltipEvent;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Vector;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@EventBusSubscriber(modid = "dimensiondelvers", value = Dist.CLIENT)
+public class RiftMap {
+    public static ArrayList<MapCell> cells = new ArrayList<>();
+    public static int x, y, z;
+
+    public static void addCell(MapCell cell) {
+        cells.add(cell);
+    }
+
+    public static void removeCell(MapCell cell) {
+        cells.remove(cell);
+    }
+
+    @SubscribeEvent(priority = EventPriority.NORMAL)
+    public static void renderMap(RenderGuiEvent.Post event) {
+        for (MapCell cell : cells) {
+            // Render cell
+        }
+
+        RenderSystem.enableBlend();
+        RenderSystem.defaultBlendFunc();
+
+        BufferBuilder buffer = Tesselator.getInstance().begin(VertexFormat.Mode.DEBUG_LINES, DefaultVertexFormat.POSITION_COLOR);
+
+
+        RenderSystem.setShader(GameRenderer::getPositionColorShader);
+
+        buffer.addVertex(0.0f, 0.0f, 0f).setColor(1f, 1f, 1f, 1f);
+        buffer.addVertex(0.0f, 5.0f, 0f).setColor(1f, 1f, 1f, 1f);
+        buffer.addVertex(5.0f, 5.0f, 0f).setColor(1f, 1f, 1f, 1f);
+        buffer.addVertex(5.0f, 0.0f, 0f).setColor(1f, 1f, 1f, 1f);
+
+
+        BufferUploader.drawWithShader(buffer.buildOrThrow());
+        RenderSystem.disableBlend();
+    }
+}
