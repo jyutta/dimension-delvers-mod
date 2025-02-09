e27ae39e0abc99b895a99cfcb4cc145a5a7a1367
a1qs <ankiller2007.09.20@gmail.com>
2025-02-09 19:36:03 +0100

    chore: register ClientTooltipComponents

--- src/main/java/com/dimensiondelvers/dimensiondelvers/events/client/ClientRegistryEvents.java
+++ src/main/java/com/dimensiondelvers/dimensiondelvers/events/client/ClientRegistryEvents.java
@@ -2,16 +2,24 @@ package com.dimensiondelvers.dimensiondelvers.events.client;
 
 import com.dimensiondelvers.dimensiondelvers.DimensionDelvers;
 import com.dimensiondelvers.dimensiondelvers.client.render.item.properties.select.SelectRuneGemShape;
+import com.dimensiondelvers.dimensiondelvers.client.tooltip.ImageTooltipRenderer;
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.RegisterClientTooltipComponentFactoriesEvent;
 import net.neoforged.neoforge.client.event.RegisterSelectItemModelPropertyEvent;
 
 @EventBusSubscriber(modid = DimensionDelvers.MODID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
 public class ClientRegistryEvents {
+
     @SubscribeEvent
     public static void registerSelectItemModelProperties(RegisterSelectItemModelPropertyEvent event) {
         event.register(ResourceLocation.fromNamespaceAndPath(DimensionDelvers.MODID, "runegem_shape"), SelectRuneGemShape.TYPE);
+    }
+
+    @SubscribeEvent
+    public static void registerClientTooltipComponents(RegisterClientTooltipComponentFactoriesEvent event) {
+        event.register(ImageTooltipRenderer.ImageComponent.class, ImageTooltipRenderer::new);
     }
 }
