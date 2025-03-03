f4fce1be27fd03c628cdeace757a49b3b83809ee
Patrigan <patrick.provinciael@gmail.com>
2025-03-03 11:39:46 +0100

    Feature: Stealth rendering.

--- src/main/java/com/wanderersoftherift/wotr/events/client/ClientRegistryEvents.java
+++ src/main/java/com/wanderersoftherift/wotr/events/client/ClientRegistryEvents.java
@@ -3,11 +3,13 @@ package com.wanderersoftherift.wotr.events.client;
 import com.wanderersoftherift.wotr.WanderersOfTheRift;
 import com.wanderersoftherift.wotr.client.render.item.properties.select.SelectRuneGemShape;
 import com.wanderersoftherift.wotr.client.tooltip.ImageTooltipRenderer;
+import com.dimensiondelvers.dimensiondelvers.gui.layer.objective.ObjectiveLayer;
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RegisterClientTooltipComponentFactoriesEvent;
+import net.neoforged.neoforge.client.event.RegisterGuiLayersEvent;
 import net.neoforged.neoforge.client.event.RegisterSelectItemModelPropertyEvent;
 
 @EventBusSubscriber(modid = WanderersOfTheRift.MODID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
@@ -21,5 +23,10 @@ public class ClientRegistryEvents {
     @SubscribeEvent
     public static void registerClientTooltipComponents(RegisterClientTooltipComponentFactoriesEvent event) {
         event.register(ImageTooltipRenderer.ImageComponent.class, ImageTooltipRenderer::new);
+    }
+
+    @SubscribeEvent
+    public static void registerGuiLayers(RegisterGuiLayersEvent event) {
+        event.registerAboveAll(DimensionDelvers.id("objective"), new ObjectiveLayer());
     }
 }
