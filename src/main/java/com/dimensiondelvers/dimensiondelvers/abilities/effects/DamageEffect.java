ec6140b90662e41c0d63dbb432906a1454f0405f
T2ThatGuy <66382380+T2ThatGuy@users.noreply.github.com>
2025-03-08 22:31:54 +0000

    feat: damage types for damage effect

--- src/main/java/com/dimensiondelvers/dimensiondelvers/abilities/effects/DamageEffect.java
+++ src/main/java/com/dimensiondelvers/dimensiondelvers/abilities/effects/DamageEffect.java
@@ -6,7 +6,11 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.damagesource.DamageType;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
@@ -16,9 +20,11 @@ import java.util.Optional;
 
 public class DamageEffect extends AbstractEffect{
     private float damageAmount = 0;
-    public DamageEffect(EffectTargeting targeting, List<AbstractEffect> effects, Optional<ParticleInfo> particles, float amount) {
+    private Holder<DamageType> damageTypeKey;
+    public DamageEffect(EffectTargeting targeting, List<AbstractEffect> effects, Optional<ParticleInfo> particles, float amount, Holder<DamageType> damageTypeKey) {
         super(targeting, effects, particles);
         this.damageAmount = amount;
+        this.damageTypeKey = damageTypeKey;
     }
 
     public static final MapCodec<DamageEffect> CODEC = RecordCodecBuilder.mapCodec(instance ->
@@ -26,9 +32,14 @@ public class DamageEffect extends AbstractEffect{
                     EffectTargeting.CODEC.fieldOf("targeting").forGetter(AbstractEffect::getTargeting),
                     Codec.list(AbstractEffect.DIRECT_CODEC).fieldOf("effects").forGetter(AbstractEffect::getEffects),
                     Codec.optionalField("particles", ParticleInfo.CODEC.codec(), true).forGetter(AbstractEffect::getParticles),
-                    Codec.FLOAT.fieldOf("amount").forGetter(DamageEffect::getAmount)
+                    Codec.FLOAT.fieldOf("amount").forGetter(DamageEffect::getAmount),
+                    DamageType.CODEC.fieldOf("damage_type").forGetter(DamageEffect::getDamageTypeKey)
             ).apply(instance, DamageEffect::new)
     );
+
+    private Holder<DamageType> getDamageTypeKey() {
+        return damageTypeKey;
+    }
 
     private float getAmount() {
         return damageAmount;
@@ -42,13 +53,20 @@ public class DamageEffect extends AbstractEffect{
     @Override
     public void apply(Entity user, List<BlockPos> blocks, Player caster) {
         List<Entity> targets = getTargeting().getTargets(user, blocks, caster);
+        DamageSource damageSource = new DamageSource(
+                caster.level().registryAccess().lookupOrThrow(Registries.DAMAGE_TYPE).getOrThrow(this.damageTypeKey.getKey()),
+                null,
+                caster,
+                null
+        );
+
         applyParticlesToUser(user);
 
         for(Entity target: targets) {
             applyParticlesToTarget(target);
             if(target instanceof LivingEntity livingTarget)
             {
-                livingTarget.hurtServer((ServerLevel) user.level(), user.damageSources().generic(), damageAmount);
+                livingTarget.hurtServer((ServerLevel) user.level(), damageSource, damageAmount);
             }
             //Then apply children affects to targets
             super.apply(target, getTargeting().getBlocks(user), caster);
