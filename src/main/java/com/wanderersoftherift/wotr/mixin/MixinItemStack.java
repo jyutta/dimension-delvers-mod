312ba17bf1fcfd546375c5cbf360f7eedf2cc1d5
a1qs <ankiller2007.09.20@gmail.com>
2025-03-16 15:27:56 +0100

    feat: add a tag to make items have durability again

--- src/main/java/com/wanderersoftherift/wotr/mixin/MixinItemStack.java
+++ src/main/java/com/wanderersoftherift/wotr/mixin/MixinItemStack.java
@@ -1,5 +1,6 @@
 package com.wanderersoftherift.wotr.mixin;
 
+import com.wanderersoftherift.wotr.init.ModTags;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +12,9 @@ public abstract class MixinItemStack {
 
     @Inject(method = "isDamageableItem", at = @At("HEAD"), cancellable = true)
     private void preventDurability(CallbackInfoReturnable<Boolean> cir) {
-        cir.setReturnValue(false); // We may want certain items to be excluded, which can be done here, currently all items are targeted
+        ItemStack stack = ((ItemStack) (Object) this);
+        if(!stack.is(ModTags.Items.UNBREAKABLE_EXCLUSIONS)) {
+            cir.setReturnValue(false); // We may want certain items to be excluded, which can be done here, currently all items are targeted that do not have the UNBREAKABLE_EXCLUSIONS tag
+        }
     }
 }
--- src/main/java/com/wanderersoftherift/wotr/mixin/MixinSetItemDamageFunction.java
+++ src/main/java/com/wanderersoftherift/wotr/mixin/MixinSetItemDamageFunction.java
@@ -1,5 +1,6 @@
 package com.wanderersoftherift.wotr.mixin;
 
+import com.wanderersoftherift.wotr.init.ModTags;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.functions.SetItemDamageFunction;
@@ -12,10 +13,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class MixinSetItemDamageFunction {
 
     // Prevents the log from being spammed whenever this runs
-    // isDamageableItem currently always returns false with the no-durability implementation
-    // so this method running is unneccessary currently
     @Inject(method = "run", at = @At("HEAD"), cancellable = true)
     private void preventConsoleSpam(ItemStack stack, LootContext context, CallbackInfoReturnable<ItemStack> cir) {
-        cir.setReturnValue(stack);
+        if(!stack.is(ModTags.Items.UNBREAKABLE_EXCLUSIONS)) {
+            cir.setReturnValue(stack);
+        }
     }
 }
