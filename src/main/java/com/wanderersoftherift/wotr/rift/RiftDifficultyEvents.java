b64e590b4d782dd1e1d54ff746f3ef8b4e5bc481
Patrigan <patrick.provinciael@gmail.com>
2025-04-25 11:30:04 +0200

    feat: add some basic difficulty increases. Not tested for balance.

---
+++ src/main/java/com/wanderersoftherift/wotr/rift/RiftDifficultyEvents.java
@@ -0,0 +1,57 @@
+package com.wanderersoftherift.wotr.rift;
+
+import com.wanderersoftherift.wotr.WanderersOfTheRift;
+import com.wanderersoftherift.wotr.core.rift.RiftData;
+import net.minecraft.core.Holder;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.player.Player;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
+
+@EventBusSubscriber(modid = WanderersOfTheRift.MODID, bus = EventBusSubscriber.Bus.GAME)
+public class RiftDifficultyEvents {
+
+    @SubscribeEvent
+    public static void onEntitySpawned(EntityJoinLevelEvent event) {
+        if (event.getLevel().isClientSide() || event.getEntity() instanceof Player) {
+            return;
+        }
+        if (event.getLevel() instanceof ServerLevel serverLevel && RiftData.isRift(serverLevel)
+                && event.getEntity() instanceof LivingEntity livingEntity) {
+            applyDifficultyToEntity(livingEntity, serverLevel);
+        }
+    }
+
+    private static void applyDifficultyToEntity(LivingEntity livingEntity, ServerLevel serverLevel) {
+        int tier = RiftData.get(serverLevel).getTier();
+        if (livingEntity instanceof Mob mob) {
+            updateAttribute(mob, Attributes.ATTACK_DAMAGE, getDamageMultiplier(tier));
+            updateAttribute(mob, Attributes.MAX_HEALTH, getHealthMultiplier(tier));
+            updateAttribute(mob, Attributes.MOVEMENT_SPEED, getSpeedMultiplier(tier));
+        }
+    }
+
+    private static void updateAttribute(Mob mob, Holder<Attribute> attackDamage, double tier) {
+        if (mob.getAttribute(attackDamage) == null) {
+            return;
+        }
+        mob.getAttribute(attackDamage).setBaseValue(mob.getAttribute(attackDamage).getBaseValue() * tier);
+    }
+
+    private static double getSpeedMultiplier(int tier) {
+        return tier * 0.02 + 1;
+    }
+
+    private static double getHealthMultiplier(int tier) {
+        return tier * 0.2 + 1;
+    }
+
+    private static double getDamageMultiplier(int tier) {
+        return tier * 0.1 + 1;
+    }
+}
