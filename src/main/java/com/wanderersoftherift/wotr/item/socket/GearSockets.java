d37ed3f06742c44f6041b66f3aead09938feb2d9
Chris Tufts <tuftssc@gmail.com>
2025-04-25 22:19:01 -0400

    adds support for socketing vanilla tools as well as 4 separate architype based tags these items in those tags will need to be balanced with implicits adds ability to set a minimum value for sockets with helper adds utility to fetch a random item based on a tag

--- src/main/java/com/wanderersoftherift/wotr/item/socket/GearSockets.java
+++ src/main/java/com/wanderersoftherift/wotr/item/socket/GearSockets.java
@@ -3,13 +3,19 @@ package com.wanderersoftherift.wotr.item.socket;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.wanderersoftherift.wotr.init.ModDataComponentType;
+import net.minecraft.core.Holder;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static com.wanderersoftherift.wotr.init.ModTags.Items.SOCKETABLE;
 
@@ -35,7 +41,7 @@ public record GearSockets(List<GearSocket> sockets) {
     public static GearSockets randomSockets(int minSockets, int maxSockets, RandomSource random) {
         List<GearSocket> sockets = new ArrayList<>();
         // sort of pulling in random.nextIntBetweenInclusive, but it didn't work exactly the way I needed it to
-        int actualSockets = random.nextInt(maxSockets - minSockets) + minSockets;
+        int actualSockets = random.nextIntBetweenInclusive(minSockets, maxSockets);
         for (int i = 0; i < actualSockets; i++) {
             GearSocket socket = GearSocket.getRandomSocket(random);
             sockets.add(socket);
---
+++ src/main/java/com/wanderersoftherift/wotr/util/ItemTagUtil.java
@@ -0,0 +1,42 @@
+package com.wanderersoftherift.wotr.util;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.Holder;
+import net.minecraft.util.RandomSource;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ItemTagUtil {
+
+    public static TagKey<Item> findTagWithPrefixOnItemStack(ItemStack itemStack, String prefix){
+        for (TagKey<Item> tag : itemStack.getTags().toList()) {
+            ResourceLocation tagLocation = tag.location();
+            if ( tagLocation.getPath().startsWith(prefix)) {
+                return tag;
+            }
+        }
+        return null;
+    }
+
+    public static ItemStack getRandomItemStackFromTag(TagKey<Item> tag, RandomSource random) {
+        // Get all items in the tag
+        List<Item> itemsInTag = getItemsFromItemTag(tag);
+        Item randomItem = itemsInTag.get(random.nextInt(itemsInTag.size()));
+        return new ItemStack(randomItem);
+    }
+
+    public static List<Item> getItemsFromItemTag(TagKey<Item> tag) {
+        return BuiltInRegistries.ITEM.get(tag)
+                .map(it -> it.stream()
+                        .map(Holder::value)
+                        .toList())
+                .orElseGet(List::of);
+    }
+
+
+}
