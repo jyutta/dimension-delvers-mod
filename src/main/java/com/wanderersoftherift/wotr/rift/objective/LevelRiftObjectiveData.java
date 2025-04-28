d5ccc39f40a6f8bfa79880a03fc6c2720803b882
Patrigan <patrick.provinciael@gmail.com>
2025-04-28 21:49:50 +0200

    fix: Parse failure on missing objective (overworld)

--- src/main/java/com/wanderersoftherift/wotr/rift/objective/LevelRiftObjectiveData.java
+++ src/main/java/com/wanderersoftherift/wotr/rift/objective/LevelRiftObjectiveData.java
@@ -51,6 +51,9 @@ public class LevelRiftObjectiveData extends SavedData {
     }
 
     public static LevelRiftObjectiveData load(CompoundTag tag, HolderLookup.Provider lookupProvider) {
+        if(!tag.contains("objective")) {
+            return new LevelRiftObjectiveData(null);
+        }
         AbstractObjective objective = AbstractObjective.DIRECT_CODEC.parse(NbtOps.INSTANCE, tag.get("objective"))
                 .resultOrPartial(LOGGER::error)
                 .orElse(null);
