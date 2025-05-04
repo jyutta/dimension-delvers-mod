4952a75001df048c1ad02b36acd40d8f1d9c99b2
Chris Tufts <tuftssc@gmail.com>
2025-05-04 07:20:24 -0400

    modifies how we are adding sockets to gear from ItemStack mixin to Item Mixin. Adds back a more generic socketable tag to support this, and adds stone tools to main hand socketable tools. also adds a min and max socket to implicitly call on itemstack in case we need it later as well as checks to make sure there aren't sockets on the gear before rolling it.

--- src/main/java/com/wanderersoftherift/wotr/mixin/MixinItemStack.java
+++ src/main/java/com/wanderersoftherift/wotr/mixin/MixinItemStack.java
@@ -1,14 +1,10 @@
 package com.wanderersoftherift.wotr.mixin;
 
 import com.wanderersoftherift.wotr.init.ModTags;
-import com.wanderersoftherift.wotr.item.socket.GearSockets;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ItemStack.class)
@@ -21,17 +17,5 @@ public abstract class MixinItemStack {
             cir.setReturnValue(false); // We may want certain items to be excluded, which can be done here, currently
                                        // all items are targeted that do not have the UNBREAKABLE_EXCLUSIONS tag
         }
-    }
-
-    @Inject(method = "onCraftedBy", at = @At("TAIL"))
-    public void onCraftedByGenerateDataComponents(Level level, Player player, int amount, CallbackInfo ci) {
-        ItemStack stack = (ItemStack) (Object) this;
-        GearSockets.generateForItem(stack, level, player);
-    }
-
-    @Inject(method = "onCraftedBySystem", at = @At("TAIL"))
-    public void onCraftedBySystemGenerateDataComponents(Level level, CallbackInfo ci) {
-        ItemStack stack = (ItemStack) (Object) this;
-        GearSockets.generateForItem(stack, level, null);
     }
 }
