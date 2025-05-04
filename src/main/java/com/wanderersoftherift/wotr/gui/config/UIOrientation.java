d263f372dec2811f1f6e33fb56760a376aa799db
immortius <immortius@gmail.com>
2025-05-04 23:15:53 +1000

    feat: rotation support for mana bar

--- src/main/java/com/wanderersoftherift/wotr/gui/config/UIOrientation.java
+++ src/main/java/com/wanderersoftherift/wotr/gui/config/UIOrientation.java
@@ -1,13 +1,26 @@
 package com.wanderersoftherift.wotr.gui.config;
 
+import org.joml.Vector2i;
+import org.joml.Vector2ic;
+
 /**
  * Orientation to render a UI element with
  */
 public enum UIOrientation {
-    HORIZONTAL,
-    VERTICAL;
+    HORIZONTAL(new Vector2i(1, 0)),
+    VERTICAL(new Vector2i(0, 1));
+
+    private final Vector2i axis;
+
+    UIOrientation(Vector2i axis) {
+        this.axis = axis;
+    }
 
     public UIOrientation next() {
         return UIOrientation.values()[(ordinal() + 1) % UIOrientation.values().length];
+    }
+
+    public Vector2ic axis() {
+        return axis;
     }
 }
