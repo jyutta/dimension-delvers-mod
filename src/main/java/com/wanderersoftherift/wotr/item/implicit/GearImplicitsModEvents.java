52086ede29f0261205aac44dd2cdc3f019d551ce
Patrigan <patrick.provinciael@gmail.com>
2025-05-09 22:57:18 +0200

    feat: Prepare adding implicits to all armors

--- src/main/java/com/wanderersoftherift/wotr/item/implicit/GearImplicitsModEvents.java
+++ src/main/java/com/wanderersoftherift/wotr/item/implicit/GearImplicitsModEvents.java
@@ -26,6 +26,7 @@ public class GearImplicitsModEvents {
                 builder -> builder.set(ModDataComponentType.GEAR_IMPLICITS.get(), new UnrolledGearImplicits()));
         event.modify(Items.NETHERITE_SWORD,
                 builder -> builder.set(ModDataComponentType.GEAR_IMPLICITS.get(), new UnrolledGearImplicits()));
+
         event.modify(Items.WOODEN_AXE,
                 builder -> builder.set(ModDataComponentType.GEAR_IMPLICITS.get(), new UnrolledGearImplicits()));
         event.modify(Items.STONE_AXE,
@@ -38,5 +39,33 @@ public class GearImplicitsModEvents {
                 builder -> builder.set(ModDataComponentType.GEAR_IMPLICITS.get(), new UnrolledGearImplicits()));
         event.modify(Items.NETHERITE_AXE,
                 builder -> builder.set(ModDataComponentType.GEAR_IMPLICITS.get(), new UnrolledGearImplicits()));
+
+        event.modify(Items.LEATHER_HELMET, builder -> builder.set(ModDataComponentType.GEAR_IMPLICITS.get(), new UnrolledGearImplicits()));
+        event.modify(Items.CHAINMAIL_HELMET, builder -> builder.set(ModDataComponentType.GEAR_IMPLICITS.get(), new UnrolledGearImplicits()));
+        event.modify(Items.IRON_HELMET, builder -> builder.set(ModDataComponentType.GEAR_IMPLICITS.get(), new UnrolledGearImplicits()));
+        event.modify(Items.GOLDEN_HELMET, builder -> builder.set(ModDataComponentType.GEAR_IMPLICITS.get(), new UnrolledGearImplicits()));
+        event.modify(Items.DIAMOND_HELMET, builder -> builder.set(ModDataComponentType.GEAR_IMPLICITS.get(), new UnrolledGearImplicits()));
+        event.modify(Items.NETHERITE_HELMET, builder -> builder.set(ModDataComponentType.GEAR_IMPLICITS.get(), new UnrolledGearImplicits()));
+
+        event.modify(Items.LEATHER_CHESTPLATE, builder -> builder.set(ModDataComponentType.GEAR_IMPLICITS.get(), new UnrolledGearImplicits()));
+        event.modify(Items.CHAINMAIL_CHESTPLATE, builder -> builder.set(ModDataComponentType.GEAR_IMPLICITS.get(), new UnrolledGearImplicits()));
+        event.modify(Items.IRON_CHESTPLATE, builder -> builder.set(ModDataComponentType.GEAR_IMPLICITS.get(), new UnrolledGearImplicits()));
+        event.modify(Items.GOLDEN_CHESTPLATE, builder -> builder.set(ModDataComponentType.GEAR_IMPLICITS.get(), new UnrolledGearImplicits()));
+        event.modify(Items.DIAMOND_CHESTPLATE, builder -> builder.set(ModDataComponentType.GEAR_IMPLICITS.get(), new UnrolledGearImplicits()));
+        event.modify(Items.NETHERITE_CHESTPLATE, builder -> builder.set(ModDataComponentType.GEAR_IMPLICITS.get(), new UnrolledGearImplicits()));
+
+        event.modify(Items.LEATHER_LEGGINGS, builder -> builder.set(ModDataComponentType.GEAR_IMPLICITS.get(), new UnrolledGearImplicits()));
+        event.modify(Items.CHAINMAIL_LEGGINGS, builder -> builder.set(ModDataComponentType.GEAR_IMPLICITS.get(), new UnrolledGearImplicits()));
+        event.modify(Items.IRON_LEGGINGS, builder -> builder.set(ModDataComponentType.GEAR_IMPLICITS.get(), new UnrolledGearImplicits()));
+        event.modify(Items.GOLDEN_LEGGINGS, builder -> builder.set(ModDataComponentType.GEAR_IMPLICITS.get(), new UnrolledGearImplicits()));
+        event.modify(Items.DIAMOND_LEGGINGS, builder -> builder.set(ModDataComponentType.GEAR_IMPLICITS.get(), new UnrolledGearImplicits()));
+        event.modify(Items.NETHERITE_LEGGINGS, builder -> builder.set(ModDataComponentType.GEAR_IMPLICITS.get(), new UnrolledGearImplicits()));
+
+        event.modify(Items.LEATHER_BOOTS, builder -> builder.set(ModDataComponentType.GEAR_IMPLICITS.get(), new UnrolledGearImplicits()));
+        event.modify(Items.CHAINMAIL_BOOTS, builder -> builder.set(ModDataComponentType.GEAR_IMPLICITS.get(), new UnrolledGearImplicits()));
+        event.modify(Items.IRON_BOOTS, builder -> builder.set(ModDataComponentType.GEAR_IMPLICITS.get(), new UnrolledGearImplicits()));
+        event.modify(Items.GOLDEN_BOOTS, builder -> builder.set(ModDataComponentType.GEAR_IMPLICITS.get(), new UnrolledGearImplicits()));
+        event.modify(Items.DIAMOND_BOOTS, builder -> builder.set(ModDataComponentType.GEAR_IMPLICITS.get(), new UnrolledGearImplicits()));
+        event.modify(Items.NETHERITE_BOOTS, builder -> builder.set(ModDataComponentType.GEAR_IMPLICITS.get(), new UnrolledGearImplicits()));
     }
 }
